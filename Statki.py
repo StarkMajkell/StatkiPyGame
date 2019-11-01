8bd6b54da45afb6d5ebbb4c44bea4afe7502a51f
StarkMajkell <48209758+StarkMajkell@users.noreply.github.com>
2019-11-01 19:46:59 +0100

    lel

--- Statki.py
+++ Statki.py
@@ -1,17 +1,42 @@
 import pygame
 import sys
 
-pygame.init() #musi być
+pygame.init()  # musi być
+#górna belka
+pygame.display.set_caption('Statki')
+a = pygame.image.load('Logo.png')
+pygame.display.set_icon(a)
 
-wielkoscOkna = (900,900)
+# moduł ze zmiennymi
+wysokoscOkna = 600
+szerokoscOkna = 1360
+rozmiargracza_1 = 50
+rozmiargracza_2 = 50
+rozmiarpocisku_1 = 10
+rozmiarpocisku_2 = 10
+kolorstatku1 = 255
+kolorstatku2 = 255
+rozmiarOkna = (szerokoscOkna, wysokoscOkna)
+obraz = pygame.display.set_mode(rozmiarOkna)
+statek_1 = pygame.Rect(0, wysokoscOkna / 2, rozmiargracza_1, rozmiargracza_1)
+statek_2 = pygame.Rect(szerokoscOkna - rozmiargracza_2, (wysokoscOkna / 2), rozmiargracza_2, rozmiargracza_2)
+pocisk_1 = pygame.Rect(szerokoscOkna, 50, rozmiarpocisku_1, rozmiarpocisku_1)
+pocisk_2 = pygame.Rect(50, wysokoscOkna, rozmiarpocisku_2, rozmiarpocisku_2)
 
-def granicePlanszy(pozycja):
-    if pozycja >= 900:
+
+# moduł funkcji
+def granicePlanszyX(pozycja):
+    if pozycja >= szerokoscOkna:
         pozycja = 0
     if pozycja < 0:
-        pozycja = 900
+        pozycja = szerokoscOkna
     return pozycja
-
+def granicePlanszyY(pozycja):
+    if pozycja >= wysokoscOkna:
+        pozycja = 0
+    if pozycja < 0:
+        pozycja = wysokoscOkna
+    return pozycja
 def ruchxAD():
     if pygame.key.get_pressed()[pygame.K_a]:
         return -1
@@ -47,50 +72,44 @@ def off():
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             sys.exit(0)
 
-obraz = pygame.display.set_mode(wielkoscOkna)
-statek_1 = pygame.Rect(50,450,50,50)
-statek_2 = pygame.Rect(800,450,50,50)
-pocisk_1 = pygame.Rect(901,50,10,10)
-pocisk_2 = pygame.Rect(50,901,10,10)
-x = 10
+
+# pętla programu program
 while True:
-    strzał_1 = 0
-    strzał_2 = 0
     obraz.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN and event.key == pygame.K_f:
-            strzał_1 = 1
-            pocisk_1.x = statek_1.x + 20
-            pocisk_1.y = statek_1.y + 20
+            pocisk_1.x = statek_1.x + rozmiargracza_1 / 2
+            pocisk_1.y = statek_1.y + rozmiargracza_1 / 2 - rozmiarpocisku_1 / 2
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
-            strzał_2 = 2
-            pocisk_2.x = statek_2.x + 20
-            pocisk_2.y = statek_2.y + 20
-    if strzał_1 == 1 or pocisk_1.x < 900:
-        pygame.draw.rect(obraz, (255, 0, 0), pocisk_1)
-        pocisk_1.x += 1
-    if strzał_2 == 2 or pocisk_2.x < 900:
-        pygame.draw.rect(obraz, (0, 0, 255), pocisk_2)
-        pocisk_2.x -= 1
-    if (statek_1.x + 50 > pocisk_2.x and statek_1.x < pocisk_2.x)and\
-            (statek_1.y + 50 > pocisk_2.y and statek_1.y < pocisk_2.y):
-        statek_1.x = 0
-        statek_1.y = 0
-    if (statek_2.x + 50 > pocisk_1.x and statek_2.x < pocisk_1.x)and\
-            (statek_2.y + 50 > pocisk_1.y and statek_2.y < pocisk_1.y):
-        statek_2.x = 850
-        statek_2.y = 0
+            pocisk_2.x = statek_2.x + rozmiargracza_2 / 2
+            pocisk_2.y = statek_2.y + rozmiargracza_2 / 2 - rozmiarpocisku_2 / 2
 
+    if pocisk_1.x < szerokoscOkna:
+        pygame.draw.rect(obraz, (255, 0, 0), pocisk_1)
+        pocisk_1.x += 2
+    if pocisk_2.x < szerokoscOkna:
+        pygame.draw.rect(obraz, (0, 0, 255), pocisk_2)
+        pocisk_2.x -= 2
+    if (statek_1.x + rozmiargracza_1 > pocisk_2.x and statek_1.x < pocisk_2.x) and \
+            (statek_1.y + rozmiargracza_1 > pocisk_2.y and statek_1.y < pocisk_2.y):
+        kolorstatku1 -= 25
+        pocisk_2 = pygame.Rect(50, wysokoscOkna, rozmiarpocisku_2, rozmiarpocisku_2)
+    if (statek_2.x + rozmiargracza_2 > pocisk_1.x and statek_2.x < pocisk_1.x) and \
+            (statek_2.y + rozmiargracza_2 > pocisk_1.y and statek_2.y < pocisk_1.y):
+        kolorstatku2 -= 25
+        pocisk_1 = pygame.Rect(szerokoscOkna, 50, rozmiarpocisku_1, rozmiarpocisku_1)
+    # poruszanie sie
     statek_1.x += ruchxAD()
     statek_1.y += ruchyWS()
     statek_2.x += ruchxLR()
     statek_2.y += ruchyUD()
-    statek_1.x = granicePlanszy(statek_1.x)
-    statek_1.y = granicePlanszy(statek_1.y)
-    statek_2.x = granicePlanszy(statek_2.x)
-    statek_2.y = granicePlanszy(statek_2.y)
+    # teleportacja na granicach mapy
+    statek_1.x = granicePlanszyX(statek_1.x)
+    statek_1.y = granicePlanszyY(statek_1.y)
+    statek_2.x = granicePlanszyX(statek_2.x)
+    statek_2.y = granicePlanszyY(statek_2.y)
 
-    pygame.draw.rect(obraz, (255, 0, 0), statek_1)
-    pygame.draw.rect(obraz, (0, 0, 255), statek_2)
+    pygame.draw.rect(obraz, (kolorstatku1, 0, 0), statek_1)
+    pygame.draw.rect(obraz, (0, 0, kolorstatku2), statek_2)
     pygame.display.flip()
     off()
