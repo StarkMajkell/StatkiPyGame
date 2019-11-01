9921f84f958d5fb5104b9e8ff56a74faecfece3c
StarkMajkell <48209758+StarkMajkell@users.noreply.github.com>
2019-11-01 23:27:25 +0100

    New

--- Statki.py
+++ Statki.py
@@ -84,34 +84,11 @@ def wynikNapis(killRed,killBlue):
 background = pygame.image.load("background.jpg").convert()
 statekGrafika_1 = pygame.image.load("Statek1-Blue.png")
 statekGrafika_2 = pygame.image.load("Statek1-Red.png")
+bonusy_speed = pygame.image.load("Speed.png")
 
 # pętla programu program
 while True:
-    #obraz.fill((0, 0, 0))
-    # wynik tekst
 
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_f:
-            pocisk_1.x = statek_1.x + rozmiargracza_1 / 2
-            pocisk_1.y = statek_1.y + rozmiargracza_1 / 2 - rozmiarpocisku_1 / 2
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
-            pocisk_2.x = statek_2.x + rozmiargracza_2 / 2
-            pocisk_2.y = statek_2.y + rozmiargracza_2 / 2 - rozmiarpocisku_2 / 2
-
-    if pocisk_1.x < szerokoscOkna:
-        pygame.draw.rect(obraz, (255, 0, 0), pocisk_1)
-        pocisk_1.x += 2
-    if pocisk_2.x < szerokoscOkna:
-        pygame.draw.rect(obraz, (0, 0, 255), pocisk_2)
-        pocisk_2.x -= 2
-    if (statek_1.x + rozmiargracza_1 > pocisk_2.x and statek_1.x < pocisk_2.x) and \
-            (statek_1.y + rozmiargracza_1 > pocisk_2.y and statek_1.y < pocisk_2.y):
-        kolorstatku1 -= 25
-        pocisk_2 = pygame.Rect(50, wysokoscOkna, rozmiarpocisku_2, rozmiarpocisku_2)
-    if (statek_2.x + rozmiargracza_2 > pocisk_1.x and statek_2.x < pocisk_1.x) and \
-            (statek_2.y + rozmiargracza_2 > pocisk_1.y and statek_2.y < pocisk_1.y):
-        kolorstatku2 -= 25
-        pocisk_1 = pygame.Rect(szerokoscOkna, 50, rozmiarpocisku_1, rozmiarpocisku_1)
     # poruszanie sie
     statek_1.x += ruchxAD()
     statek_1.y += ruchyWS()
@@ -124,11 +101,11 @@ while True:
     statek_2.y = granicePlanszyY(statek_2.y)
 
     wynikNapis(killRed,killBlue)
-    #pygame.draw.rect(obraz, (kolorstatku1, 0, 0), statek_1)
-   # pygame.draw.rect(obraz, (0, 0, kolorstatku2), statek_2)
     pygame.display.flip()
     clock.tick(clock_tick_rate)
     obraz.blit(background, [0, 0])
+    obraz.blit(bonusy_speed, [100, 100])
     obraz.blit(statekGrafika_1, [statek_1.x, statek_1.y])
     obraz.blit(statekGrafika_2, [statek_2.x, statek_2.y])
+
     off()
